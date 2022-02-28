e82ac6058b8e504acde1a0c8dc376053ef6e5839
raihanhd12 <fashfour487@gmail.com>
2022-02-28 09:47:33 +0700

    first commit

---
+++ resources/views/raihan1.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Halaman Normal
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>BIODATA RHD</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+   <!-- Buat HP
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- File CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/default.css">
+	<link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">
+   <link rel="stylesheet" href="css/magnific-popup.css">
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+</head>
+
+<body>
+
+   <!-- Header
+   ================================================== -->
+   <header id="home">
+
+      <nav id="nav-wrap">
+
+         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" class="nav">
+            <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+            <li><a class="smoothscroll" href="#about">About Me</a></li>
+	         <li><a class="smoothscroll" href="#resume">Education</a></li>           
+         </ul> <!-- end #nav -->
+
+      </nav> <!-- end #nav-wrap -->
+
+      <div class="row banner">
+         <div class="banner-text">
+            <h1 class="responsive-headline">Raihan Hidayatullah</h1>
+            <h1 class="responsive-headline">Djunaedi</h1>
+            <h3><span>Seorang mahasiswa Teknologi Informasi yang bercita - cita memiliki perusahaan terbesar di bidang IT. 
+            </span> <a class="smoothscroll" href="#about"><br><br><a class="ganti">~ Be yourself. An original is so much better than a copy ~</a></br></br></a></h3>
+            <hr />
+            <ul class="social">
+               <li><a href="https://www.facebook.com/RaihanHidayatullahD"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>              
+               <li><a href="https://www.linkedin.com/in/raihan-d-320a1412a/"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="https://www.instagram.com/raaihan.n/"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+         </div>
+      </div>
+
+      <p class="scrolldown">
+         <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+      </p>
+
+   </header> <!-- Header End -->
+
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row">
+
+         <div class="three columns">
+
+            <img class="profile-pic"  src="images/raihan4.jpg" alt="" />
+
+         </div>
+
+         <div class="nine columns main-col">
+
+            <h2>About Me</h2>
+
+            <p>Perkenalkan saya Raihan Hidayatullah Djunaedi. Biasa dipanggil Raihan, Seorang pria yang suka bermain game dan juga mendengarkan musik.
+            Mahasiswa Teknologi Informasi yang antusias dan bermotivasi tinggi untuk mencapai cita - citanya.
+            </p>
+
+            <div class="row">
+
+               <div class="columns contact-details">
+
+                  <h2>Contact</h2>
+                  <p class="address">
+						   <span>Raihan Hidayatullah Djunaedi</span><br>
+						   <span>Jl. Puspa VI No.32 Mekarmukti, Kec. Cikarang Utara,<br>
+                     Kab. Bekasi, Jawa Barat 17530
+                     </span><br>
+						   <span>081906781639</span><br>
+                     <span>itsmehannn@gmail.com</span>
+					   </p>
+
+               </div>
+
+               <div class="columns download">
+                  <p>
+                     <a href="selengkapnya" class="button"></i>Selengkapnya</a>
+                  </p>
+               </div>
+
+            </div> <!-- end row -->
+
+         </div> <!-- end .main-col -->
+
+      </div>
+
+   </section> <!-- About Section End-->
+
+
+   <!-- Resume Section
+   ================================================== -->
+   <section id="resume">
+
+      <!-- Education
+      ----------------------------------------------- -->
+      <div class="row education">
+
+         <div class="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>TK PEPITO KIDS SCHOOL</h3>
+                  <p class="info">Playgroup - TK<span>&bull;</span> <em class="date">2006 - 2008</em></p>
+
+                  <p>
+                  TK PEPITO KIDS SCHOOL Kab. Bekasi merupakan salah satu pilihan sekolah tk yang ada di Kab. Bekasi.
+                  Alamat di Jl. Azalea Raya Ruko Melawai Blok A-26 Taman Lembah Hijau Serang Kec. Cikarang Selatan Kab. Bekasi Prov. Jawa Barat.
+                  Dikarenakan saya saat itu tinggal di Lembah Hijau maka saya Playgroup sampai TK di TK Pepito tersebut dikarenakan dekat dari rumah.                  
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>SD Karya Iman</h3>
+                  <p class="info">Kelas 1 - 6<span>&bull;</span> <em class="date">2009 - 2014</em></p>
+
+                  <p>
+                  Saya menempuh pendidikan Sekolah Dasar di SD Karya Iman yang beralamat di Jln Cemara Raya Meadow Green, Lippo Cikarang.
+                  Dikarenakan kita satu keluarga pindah rumah ke daerah meadow green maka dari itu saya menempuh pendidikan sekolah dasar di SD Karya Iman.
+                  Dekat dari rumah dan fasilitasnya lumayan bagus, selama 6 tahun disana saya memiliki banyak teman di sekitar rumah. Dikarenakan banyak murid bahkan teman sekelas
+                  yang tinggal di sekitar rumah saya
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>SMP Presiden</h3>
+                  <p class="info">7 - 9 <span>&bull;</span> <em class="date">2015 - 2017</em></p>
+
+                  <p>
+                  SMP PRESIDEN adalah salah satu satuan pendidikan dengan jenjang SMP di Simpangan, Kec. Cikarang Utara, Kab. Bekasi, Jawa Barat.
+                   Dalam menjalankan kegiatannya, SMP PRESIDEN berada di bawah naungan Kementerian Pendidikan dan Kebudayaan. Saya menempuh pendidikan Menengah Pertama di
+                   SMP Presiden, yang beralamatkan di Jababeka. Dikarenakan orang tua saya mencari tempat tinggal yang lebih dekat dengan akses tol maka dari itu kita sekeluarga pindah
+                   ke Jababeka, dimana dekat juga dengan sekolah saya. di SMP Presiden ini ada hal istimewanya menurut saya yaitu peraturan Black Book. Dimana ketika kita melakukakan pelanggaran
+                   kita menulis pelanggaran kita di buku Black Book, Yang akan mempengaruhi nilai akademik yaitu pelajaran Leadership.
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3>SMA Al Muslim</h3>
+                  <p class="info">10 - 12<span>&bull;</span> <em class="date">2018 - 2020</em></p>
+
+                  <p>
+                  SMA Al Muslim beralamatkan di Tambun, Bekasi. Saya menempuh pendidikan menengah atas di SMA Al Muslim. Dikarenakan saya ingin menemukan suasana baru
+                  yaitu lebih memperdalam ilmu agama saya, maka dari itu saya bersekolah di SMA Al Muslim. Sekolah ini jauh dari rumah saya tetapi disekolah ini kita diperbolehkan
+                  membawa kendaraan. Ada yang membawa mobil atau motor dan juga ada jemputuan khusus untuk daerah jauh seperti Lippo Cikarang dan Jababeka. Pada Kelas 10 karena awal
+                  jadi saya menggunakan jemputan selanjutnya kelas 11 sampai 12 saya membawa kendaraan motor sendiri.
+                  </p>
+
+               </div>
+
+            </div> <!-- item end -->
+
+         </div> <!-- main-col end -->
+
+      </div> <!-- End Education -->
+
+   </section> <!-- Resume Section End-->
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="social-links">
+               <li><a href="https://www.facebook.com/RaihanHidayatullahD"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>               
+               <li><a href="https://www.linkedin.com/in/raihan-d-320a1412a/"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="https://www.instagram.com/raaihan.n/"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+
+            <ul class="copyright">
+               <li>&copy; Copyright Februari 2022 By RHD</li>                
+            </ul>
+
+         </div>
+
+         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/waypoints.js"></script>
+   <script src="js/jquery.fittext.js"></script>
+   <script src="js/magnific-popup.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
